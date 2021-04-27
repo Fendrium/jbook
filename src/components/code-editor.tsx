7f118fe8fbf4a076310c22c98eb46cdb76e291cb
Charlie NVP <charlie.nguyenvanphuc@gmail.com>
2021-04-27 21:43:24 +0900

    Customize code editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,23 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor language="javascript" theme="dark" height="500px" />;
+  return (
+    <MonacoEditor
+      language="javascript"
+      theme="dark"
+      height="500px"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
