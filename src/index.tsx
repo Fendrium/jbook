c3304f24ece60f7f95b07ff9e2603a9940185080
Charlie NVP <charlie.nguyenvanphuc@gmail.com>
2021-04-28 12:09:58 +0900

    Move code cell logic to code cell component
    Move code cell logic from src/index.tsx to src/components/code-cell.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
