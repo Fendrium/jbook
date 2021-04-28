d0e26007a5827e31a0d73df1e82248ab6dd502d1
Charlie NVP <charlie.nguyenvanphuc@gmail.com>
2021-04-28 16:40:08 +0900

    Complete resize feature of code cell
    Code cell is resizable vertically and horizontally with a  center resize handle

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,10 +16,12 @@ const CodeCell = () => {
   return (
     <Resizable direction="vertical">
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-        <CodeEditor
-          initialValue="const a = 1;"
-          onChange={(value) => setInput(value)}
-        />
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from 'react-resizable';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
 
 interface ResizableProps {
@@ -6,10 +6,27 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox height={300} width={Infinity} resizeHandles={['s']}>
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.7, Infinity],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 48],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ['s'],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 export default Resizable;
