a9340910f379d4b6c48de57811c15cc9de62aa7d
Charlie NVP <charlie.nguyenvanphuc@gmail.com>
2021-04-29 23:55:47 +0900

    Add async error handling

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -12,11 +12,17 @@ const html = `
     <body>
       <div id="root"></div>
       <script>
-      const handleError = (err) => {
-        const root = document.querySelector('#root');
-        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
-        throw err;
-      }
+        const handleError = (err) => {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+          console.error(err);
+        }
+        
+        window.addEventListener('error', (event) => {
+          event.preventDefault();
+          handleError(event.error);
+        });
+
         window.addEventListener('message', (event) => {
           try {
             eval(event.data);
