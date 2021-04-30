fc164d20c0e00153474243aa9f5b2069a603175f
Charlie NVP <charlie.nguyenvanphuc@gmail.com>
2021-04-30 09:37:46 +0900

    Implement Markdown cell and enable toggling
    Markdown cell will toggle between view mode or edit mode when clicked

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect, useRef } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+
+const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# Header'} />
+    </div>
+  );
+};
+
+export default TextEditor;
